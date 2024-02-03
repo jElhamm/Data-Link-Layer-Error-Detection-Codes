54369119db214b91e6481740b74eb04335211c76
jelhamm <elhamjafari200312@gmail.com>
2024-02-04 00:21:16 +0330

    Add Header file

---
+++ Hamming Codes/hamming.h
@@ -0,0 +1,19 @@
+// This code is a header file named 'hamming.h' that includes 
+// a function prototype 'calculateHammingCode' without a function definition.
+
+
+
+#ifndef HAMMING_H
+#define HAMMING_H
+
+void calculateHammingCode(int data[], int dataBits);
+
+#endif
+
+
+
+/**
+  * The function prototype void calculateHammingCode(int data[], int dataBits); 
+  * declares a function named calculateHammingCode that takes an array of integers data and an integer dataBits 
+  * as arguments without providing the actual implementation.
+**/
