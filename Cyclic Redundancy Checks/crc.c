2f02a8538c7858afb46953657a508c5f659c4e01
jelhamm <elhamjafari200312@gmail.com>
2024-02-07 02:04:08 +0330

    Function to calculate CRC value and generate the CRC table

---
+++ Cyclic Redundancy Checks/crc.c
@@ -0,0 +1,34 @@
+/**
+  * This code is an implementation of a CRC (Cyclic Redundancy Check) algorithm
+  *  used to generate and verify CRC values for data to detect errors during data transmission or storage. 
+**/
+
+
+
+#include "crc.h"
+#include <string.h>
+#include <stdio.h>
+
+uint32_t crcTable[256];
+
+// Function to generate the CRC table
+void generateCRCTable() {
+    for (uint32_t i = 0; i < 256; i++) {
+        uint32_t crc = i << (WIDTH - 8);                                        // Calculate CRC value for the current index
+        for (uint32_t j = 0; j < 8; j++) {                                      // Loop through 8 bits
+            crc = (crc & TOPBIT) ? ((crc << 1) ^ POLYNOMIAL) : (crc << 1);      // Perform CRC calculation
+        }
+        crcTable[i] = crc;                                                      // Store the calculated CRC value in the table
+    }
+}
+
+// Function to calculate CRC value for given data
+uint32_t calculateCRC(char *data) {
+    uint32_t crc = 0xFFFFFFFF;
+    size_t dataSize = strlen(data);
+    for (size_t i = 0; i < dataSize; i++) {                                     // Loop through each character in the input data
+        crc = (crc << 8) ^ crcTable[((crc >> (WIDTH - 8)) ^ data[i]) & 0xFF];   // Update the CRC value
+    }
+    return crc;     
+}
+ 
---
+++ Cyclic Redundancy Checks/crc.h
@@ -0,0 +1,19 @@
+// This code is a header file crc.h that defines constants, function prototypes,
+// and external variables related to a CRC (Cyclic Redundancy Check) algorithm
+
+
+
+#ifndef CRC_H
+#define CRC_H
+#include <stdint.h>
+#include <stdbool.h>
+#define WIDTH 32
+#define TOPBIT (1u << (WIDTH - 1))
+#define POLYNOMIAL 0x04C11DB7
+extern uint32_t crcTable[256];
+
+
+void generateCRCTable();
+uint32_t calculateCRC(char *data);
+
+#endif
