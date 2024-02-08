5d7ded27adb610977fd6b6395d9f442f313959f5
jelhamm <elhamjafari200312@gmail.com>
2024-02-08 22:07:03 +0330

    Calculate the checksum of the data array

---
+++ Checksums/Checksums.c
@@ -0,0 +1,29 @@
+/**
+  * This code defines two functions for checksum operations. 
+
+  * 1. `calculateChecksum`: It calculates the checksum by summing up all elements in the data array,
+  *     carrying over any overflow, and then taking the one's complement of the final sum as the checksum.
+   
+  * 2. `verifyChecksum`: It calls `calculateChecksum` and returns `1` if the calculated checksum is `0`,
+  *     indicating a valid checksum, or `0` otherwise.
+
+  * These functions are used for data integrity checks using checksums.
+**/
+
+
+
+#include "Checksums.h"
+
+
+// Calculate the checksum of the data array
+unsigned short calculateChecksum(char data[], int size) {
+    unsigned int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += data[i];
+    }
+    while (sum >> 16) {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    return (unsigned short)(~sum);
+}
+ 
---
+++ Checksums/Checksums.h
@@ -0,0 +1,14 @@
+/**
+  * This code is a header file.
+  * It declares two functions: `calculateChecksum` that calculates a checksum for the provided data,
+  *                             and `verifyChecksum` that verifies the checksum of the data.
+**/
+
+
+
+#ifndef CHECKSUMS_H
+#define CHECKSUMS_H
+
+unsigned short calculateChecksum(char data[], int size);
+
+#endif
